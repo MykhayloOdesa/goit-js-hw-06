03186b008b0239b95b0960df24f4de160bca0408
MykhayloOdesa <m.yervachov@gmail.com>
2022-12-17 13:38:14 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -16,5 +16,29 @@
 // Під час відправлення форми сторінка не повинна перезавантажуватися.
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості,
-// а значення поля - значенням властивості.Для доступу до елементів форми використовуй властивість elements.
+// а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", onButtonSubmit);
+
+function onButtonSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (!email.value || !password.value) {
+    alert("Please fill in all the fields!");
+    return;
+  }
+
+  console.log({
+    email: email.value,
+    password: password.value,
+  });
+
+  event.currentTarget.reset();
+}
